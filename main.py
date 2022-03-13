eae71c42c9be23685db402fc5349e0e660beba4b
Kuzina_Angelina <62062643+missheroin@users.noreply.github.com>
2022-03-13 21:39:09 +0200

    current text-consol version (finished)

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def format_cor(stroka):
 def format_ans(stroka):
     a = []
     for i in range(len(stroka)):
-        a.append(str(stroka[i])[2:-2])
+        a.append(str(stroka[i])[1:-2])
     a = str(a)
     a = a.replace("[", "")
     a = a.replace("]", "")
@@ -45,7 +45,6 @@ flag = True
 level = 1
 while flag and level <= 5:
     print(f'Уровень {level}')
-    a = []
     connection = sqlite3.connect("quizz.db")
     cursor = connection.cursor()
     count_quest = cursor.execute(f"""SELECT id FROM level_{level}""").fetchall()
@@ -57,15 +56,16 @@ while flag and level <= 5:
         count += 1
         question = format_osn(cursor.execute(f"""SELECT question FROM level_{level} WHERE id = {number_s[count - 1]}""").fetchall())
         ansers = format_ans(cursor.execute(f"""SELECT ans_1, ans_2, ans_3, ans_4 FROM level_{level} WHERE id = {number_s[count - 1]}""").fetchall())
-        print(f'Внимание, вопрос: {question}')
+        print(f'Внимание, вопрос №{count}: {question}')
         print(f'Варианты ответа: {ansers}')
         correct = format_cor(cursor.execute(f"""SELECT correct_ans FROM level_{level} WHERE id = {number_s[count - 1]}""").fetchall())
         anser = input()
         if anser in correct:
             print('Правильный ответ\n')
         else:
-            print('Неправильный ответ. Вы проиграли')
+            print('Неправильный ответ. К сожалению, Вы проиграли.\nВикторина окончена.')
             fl = False
+            connection.close()
     if fl == False:
         flag = False
     if flag:
@@ -75,4 +75,5 @@ while flag and level <= 5:
             count = 0
             if level == 5:
                 print('Поздравляю! Вы прошли викторину!')
+                connection.close()
             level += 1
